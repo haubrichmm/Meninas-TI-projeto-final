66deadc766ef3f569508a05b3b2c0b9190857251
haubrichmm <150727351+haubrichmm@users.noreply.github.com>
2023-12-04 17:08:35 -0300

    Add files via upload

---
+++ objetos.js
@@ -0,0 +1,62 @@
+var instrumentos = [
+    {
+        imagem: "img/violino.png",
+        titulo: "violino",
+        marca: "Amati",
+        valor: "R$800,00"
+    },
+    {
+        imagem: "img/viola.png",
+        titulo: "viola",
+        marca: "Hofma",
+        valor: "R$1000,00"
+    },
+    {
+        imagem: "img/violoncelo.png",
+        titulo: "violoncelo",
+        marca: "Eagle",
+        valor: "R$1500,00"
+    },  
+    {
+        imagem: "img/contrabaixo.png",
+        titulo: "contrabaixo",
+        marca: "Fender",
+        valor: "2500,00"
+    },
+    {
+        imagem: "img/harpa.png",
+        titulo: "harpa",
+        marca: "Antoni Marsale",
+        valor: "R$5000,00"
+    },
+    {
+        imagem: "img/trompa.png",
+        titulo: "trompa",
+        marca: "Marcus Bonna",
+        valor: "R$4500,00"
+    },
+    {
+        imagem: "img/trompete.png",
+        titulo: "trompete",
+        marca: "Yamaha",
+        valor: "R$1500,00"
+    },
+    {
+        imagem: "img/trombone.png",
+        titulo: "trombone",
+        marca: "Michael",
+        valor: "R$3000,00"
+    },
+    {
+        imagem: "img/tuba.png",
+        titulo: "tuba",
+        marca: "Eagle",
+        valor: "10000,00"
+    },
+
+
+    
+
+
+
+]
